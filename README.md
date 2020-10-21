357d80ad7dfd06fb2c7d255a64c6d34486cce754
elshie21 <70971798+elshie21@users.noreply.github.com>
2020-10-20 20:58:29 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # JavaScript-Password-Generator
+
+Summary
+the purpose of the password generator is to provide a secure password of 8 to 100 characther with the following component of each element of ;Uppercase,Lowercase, special characthers and numbers
+the password is selected randomly and will be displayed in the Generator text box
+the password generator was created with Visual studio code. 
+you'll be required to create a HTML,CSS And Javascript
+
+youll be prompted by a message to insure the password has all the folloing components
+
+How to install
+to install this code youll be able to recover it on Github in the my "password tool Generator"
---
+++ main.js
@@ -0,0 +1,54 @@
+//Password Generator
+alert ("Password Generator will provide password of 8 characthers with Uppercase,Lowercase,Symbols and Numbers")
+
+//objective is to a Generate password  of 8 characthers with Uppercase, Lowercase,Special characthers and number
+// MAX password can only be of 127 characther while MIN password must be 8 characthers long.
+const button = document.querySelector("#generate").addEventListener
+
+// Possible password outcome 
+var UppecaseChar;
+var LowercaseChar;
+var Numbers;
+var SpecialChar;
+
+UppecaseChar= ["A,B,C,D,E,F,G,H,J,K,L,M,N,O,P,Q,R,S,T,U,W,X,Z"]
+LowercaseChar=["a,b,c,d,e,f,g,j,k,l.n,o,p,q,r,s,t,u,w,x,z"]
+Numbers=["0,1,2,3,4,5,6,7,8,9"]
+SpecialChar=["@,#,$,&,^,_"]
+
+//we then loop through the selected array
+//password will be 8 characther minus 1 special characther
+
+let password ="8>"
+
+}
+    password; password + value.charAt(math.floor(math.random()) * math.floor(value.lenght-1)
+}
+
+ // display password to textbox
+ Document.getElementById("display").value = password;
+
+ var password = generatepasswprd();
+ var passwordtext= document.querySelector("#password");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
